7756b3c675503259463d0c264aebf4124b228fce
criofox <imbaac@gmail.com>
2022-05-12 20:32:54 +0300

    Add redux/thunk/axios structure

---
+++ src/api/index.js
@@ -0,0 +1,3 @@
+import axios from "axios";
+
+const API = axios.create({ baseURL: "https://api-trello-app.herokuapp.com/" });
---
+++ src/redux/thunks/user.js
@@ -0,0 +1,9 @@
+import * as api from "../../api/index.js";
+import { hideLoader, showLoader } from "../actions/actions.js";
+
+export const getUsers = (id) => async (dispatch) => {
+  try {
+  } catch (error) {
+    throw error;
+  }
+};
